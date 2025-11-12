0a9f0460052a868e213372743ce068285c82fb6f
Rutu Bhanderi <140520663+rutubhanderi@users.noreply.github.com>
2025-11-12 13:50:43 +0530

    Delete apps/web directory

--- apps/web/app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthForm } from "../../components/auth/AuthForm";
-
-export default function SignUpPage() {
-  return (
-    <div className="min-h-screen bg-slate-900 flex items-center justify-center p-4">
-      <AuthForm mode="signup" />
-    </div>
-  );
-}
